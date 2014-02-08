f0bcb18e56ef2447949ca4ffd5ba0dc3c317a398
Kathy Darling <helgatheviking@gmail.com>
2014-02-08 12:23:03 +0100

    add filter for taxonomy args

--- classes/class-woothemes-our-team-taxonomy.php
+++ classes/class-woothemes-our-team-taxonomy.php
@@ -113,7 +113,8 @@ class Woothemes_Our_Team_Taxonomy {
 	 * @return  void
 	 */
 	public function register () {
-		register_taxonomy( esc_attr( $this->token ), esc_attr( $this->post_type ), (array)$this->args );
+		$args = apply_filter( 'woothemes_our_team_taxonomy_args', (array)$this->args );
+		register_taxonomy( esc_attr( $this->token ), esc_attr( $this->post_type ), $args );
 	} // End register()
 } // End Class
 ?>
