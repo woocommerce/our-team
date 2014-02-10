c4e1b83756474ddde872e5553a44dba82e618a44
Kathy Darling <helgatheviking@gmail.com>
2014-02-10 12:21:42 +0100

    cast post type $args as array

--- classes/class-woothemes-our-team.php
+++ classes/class-woothemes-our-team.php
@@ -122,7 +122,7 @@ class Woothemes_Our_Team {
 			'menu_position' 		=> 5,
 			'menu_icon' 			=> ''
 		);
-		$args = apply_filters( 'woothemes_our_team_post_type_args', $args );
+		$args = apply_filters( 'woothemes_our_team_post_type_args', (array) $args );
 		register_post_type( $this->token, $args );
 	} // End register_post_type()
 
